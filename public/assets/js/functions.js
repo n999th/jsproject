d53bad7ea5c81c9797d295e12f290a9c4cc25c8b
mmetr14 <mmetr14@freeuni.edu.ge>
2017-06-18 21:25:27 +0400

    stf

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -1,17 +1,3 @@
-/*
-$(function() {
-	$('.sidebar .widget ul li:first-child').addClass('first');
-	$('#navigation ul li:first-child').addClass('first');
-	$('#navigation ul li:last-child').addClass('last');
-	$('.footer-nav ul li:first-child').addClass('first');
-
-	$('a.popup').colorbox({
-		onComplete: function(){
-			$.colorbox.resize();
-		}
-	})
-});
-*/
 $(document).ready(function(){
 
   $("#uploadid").on('click',function(){
