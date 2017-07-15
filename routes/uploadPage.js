ffe71c6529999eb846fa25fbd3c1ed2dca907a35
mmetr14 <mmetr14@freeuni.edu.ge>
2017-07-15 20:21:30 +0400

    small fixes

--- routes/uploadPage.js
+++ routes/uploadPage.js
@@ -1,33 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var formidable = require('formidable');
-var fs = require('fs');
-var mkdirp = require('mkdirp');
-
+var fileupload = require('../models/fileUpload');
 
 router.post("/fileupload",function(req,res){
-	var myDir =  __dirname + "/../uploads/" + req.user.username+"/";
-	
-
-	try {
-  		fs.accessSync(myDir);
-	} catch (e) {
-		mkdirp(myDir, function(err) { 
-			if(err) throw err;
-			else{
-				var form = new formidable.IncomingForm();
-				form.parse(req,function(err,fields,files){
-					var oPath = files.filetoupload.path;
-					var nPath = myDir + files.filetoupload.name;
-					fs.rename(oPath,nPath,function(err){
-						if(err) throw err;
-						res.write("File Uploaded");
-						res.end();
-					});
-				});
-			}
-		});		
-	}
+	fileupload.upload(req,res);
 });
 
 module.exports = router;
