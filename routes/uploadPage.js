5f2a9d26cfb4ecf1b0c1ea903108cbb2cccd4483
mmetr14 <mmetr14@freeuni.edu.ge>
2017-07-15 20:32:00 +0400

    refactor

--- routes/uploadPage.js
+++ routes/uploadPage.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var fileupload = require('../models/fileUpload');
+var fileupload = require('../controllers/fileUpload');
 
 router.post("/fileupload",function(req,res){
 	fileupload.upload(req,res);
