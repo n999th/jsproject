742bb58ae2330e8c2a89c4f46610c2652a2a40b8
mmetr14 <mmetr14@freeuni.edu.ge>
2017-07-15 19:09:40 +0400

    upload working

--- routes/uploadPage.js
+++ routes/uploadPage.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+
 
 router.post("/fileupload",function(req,res){
-	console.log("aeha aqa vaar");
+	var myDir =  __dirname + "/../uploads/" + req.user.username+"/";
+	
+
+	try {
+  		fs.accessSync(myDir);
+	} catch (e) {
+		mkdirp(myDir, function(err) { 
+			if(err) throw err;
+			else{
+				var form = new formidable.IncomingForm();
+				form.parse(req,function(err,fields,files){
+					var oPath = files.filetoupload.path;
+					var nPath = myDir + files.filetoupload.name;
+					fs.rename(oPath,nPath,function(err){
+						if(err) throw err;
+						res.write("File Uploaded");
+						res.end();
+					});
+				});
+			}
+		});		
+	}
 });
 
 module.exports = router;
