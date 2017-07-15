9e9a35523afc08cc385e3935a51e3df5e2dfa6fd
IKhaduri <iraklixaduri@gmail.com>
2017-07-15 19:58:56 +0400

    Beautify upload page

--- public/assets/js/uploadPageJs.js
+++ public/assets/js/uploadPageJs.js
@@ -1,42 +1,37 @@
 $(document).ready(function(){
+    $('#add-id').on('click', function(){
 
- $('#add-id').on('click', function(){
-  
-      var item = $('.form-control').find("option:selected").text();
-      if (item === "")
-        return;
-     // console.log("item is : " + item);
-      $('#tag-list').append('<li>' + item + '</li>');
-      console.log($('.form-control').find("option:selected"));
-      
-      $('.form-control').find("option:selected").remove();
-      return false;
+        var item = $('.form-control').find("option:selected").text();
+        if (item === "")
+            return;
+        $('#tag-list').append('<li>' + item + '</li>');
+        console.log($('.form-control').find("option:selected"));
+        $('.form-control').find("option:selected").remove();
+        return false;
     });
- 
- $(document).on('click', '#tag-list > li', function() {
-                var $this = $(this);
-                console.log($this.html());
-                $('.form-control').append('<option>' + $this.html() + '</option>');
-                $this.remove();
-            });
+    $(document).on('click', '#tag-list > li', function() {
+        var $this = $(this);
+        console.log($this.html());
+        $('.form-control').append('<option>' + $this.html() + '</option>');
+        $this.remove();
+    });
 
- $('#submit-id').on('click', function (argument) {
+    $('#submit-id').on('click', function (argument) {
         var text = $('#textarea-id').val();
         var tags = [];
         $('#tag-list > li').each(function(i){
             tags.push(($(this)).text()); 
         });
         $.ajax({
-        type: 'POST',
-        url: '/uploadPage/',
-        data:{text:text,tagList:tags},
-        success: function(data){
-          //do something with the data via front-end framework
-          location.reload();
-        }
-    });
-
- })
+            type: 'POST',
+            url: '/uploadPage/',
+            data:{text:text,tagList:tags},
+            success: function(data){
+              //do something with the data via front-end framework
+              location.reload();
+            }
+        });
+    })
 });
 function validate(){
     console.log("validate");
