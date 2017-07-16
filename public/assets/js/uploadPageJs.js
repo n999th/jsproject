1539e1c203cb877bab2498a918a9e5ccf12c3893
IKhaduri <iraklixaduri@gmail.com>
2017-07-17 00:21:53 +0400

    Fix upload button

--- public/assets/js/uploadPageJs.js
+++ public/assets/js/uploadPageJs.js
@@ -32,6 +32,31 @@ $(document).ready(function(){
             }
         });
     })
+;( function( $, window, document, undefined )
+{
+    $( '.inputfile' ).each( function()
+    {
+        var $input   = $( this ),
+            $label   = $input.next( 'label' ),
+            labelVal = $label.html();
+
+        $input.on( 'change', function( e )
+        {
+            var fileName = '';
+
+            if( this.files && this.files.length > 1 )
+                fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+            else if( e.target.value )
+                fileName = e.target.value.split( '\\' ).pop();
+                $label.html( fileName );
+        });
+
+        // // Firefox bug fix
+        // $input
+        // .on( 'focus', function(){ $input.addClass( 'has-focus' ); })
+        // .on( 'blur', function(){ $input.removeClass( 'has-focus' ); });
+    });
+})( jQuery, window, document );
 });
 function validate(){
     console.log("validate");
