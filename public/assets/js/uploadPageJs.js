dd606aab42258e7f49cef398a7f429c87f7ce82d
IKhaduri <iraklixaduri@gmail.com>
2017-07-18 18:14:20 +0400

    Add counter mode to tags

--- public/assets/js/uploadPageJs.js
+++ public/assets/js/uploadPageJs.js
@@ -1,35 +1,38 @@
+var count = 0;
 $(document).ready(function(){
     $('#add-id').on('click', function(){
-
         var item = $('.form-control').find("option:selected").text();
         if (item === "")
             return;
-        $('#tag-list').append('<li name ='+item+" value = " + item +'>'+ item+ '</li>');
+        $('#tag-list').append('<input type="hidden" name =tag'+ count+" value = " + item +'>'+ '<li>'+ item+ '</li>');
         $('.form-control').find("option:selected").remove();
+        count += 1;
         return false;
     });
     $(document).on('click', '#tag-list > li', function() {
+        count -=1;
         var $this = $(this);
         $('.form-control').append('<option>' + $this.html() + '</option>');
         $this.remove();
     });
 
-    $('#submit-id').on('click', function (argument) {
-        var text = $('#textarea-id').val();
-        var tags = [];
-        $('#tag-list > li').each(function(i){
-            tags.push(($(this)).text()); 
-        });
-        $.ajax({
-            type: 'POST',
-            url: '/uploadPage/',
-            data:{text:text,tagList:tags},
-            success: function(data){
-              //do something with the data via front-end framework
-              location.reload();
-            }
-        });
-    })
+    // $('#submit-id').on('click', function (argument) {
+        
+    //     // var text = $('#textarea-id').val();
+    //     // var tags = [];
+    //     // $('#tag-list > li').each(function(i){
+    //     //     tags.push(($(this)).text()); 
+    //     // });
+    //     // $.ajax({
+    //     //     type: 'POST',
+    //     //     url: '/uploadPage/',
+    //     //     data:{text:text,tagList:tags},
+    //     //     success: function(data){
+    //     //       //do something with the data via front-end framework
+    //     //       location.reload();
+    //     //     }
+    //     // });
+    // })
 ;( function(){
     $( '.inputfile' ).each( function(){
         var $input   = $( this ),
@@ -47,6 +50,7 @@ function validate(){
     availableEndings = [".doc",".docx",".pdf",".txt"];
     for(const ending of availableEndings){
         if (name.endsWith(ending)){
+            $('#tag-list').append('<input type="hidden" name =counter'+ " value = " + count +'>');
             return true;
         }
     }
