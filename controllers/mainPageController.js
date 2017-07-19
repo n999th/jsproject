2e59a04103f663fe3989d0f3636fc6cb7e204c8c
mmetr14 <mmetr14@freeuni.edu.ge>
2017-07-19 19:00:20 +0400

    added some stuff

--- controllers/mainPageController.js
+++ controllers/mainPageController.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var tagModel = require('../models/tagModel');
 var textModel = require('../models/textModel');
-
+var fs = require('fs');
 
 module.exports = function(app){
 	app.get("/",function(req,res){
@@ -26,6 +26,30 @@ module.exports = function(app){
     		res.render("uploadPage.ejs", {tags:data});
   		}).sort({popularity:-1});
 	})
+  app.get('/*.pdf',function(req,res){
+    // console.log(req.path);
+    // var stream = fs.readStream(req.path);
+    // var filename = req.path.split('\\').pop(); 
+    // // Be careful of special characters
+
+    // filename = encodeURIComponent(filename);
+    // // Ideally this should strip them
+
+    // res.setHeader('Content-disposition', 'inline; filename="' + filename + '"');
+    // res.setHeader('Content-type', 'application/pdf');
+
+    // stream.pipe(res);
+    let readStream = fs.createReadStream(__dirname +"/.."+ req.path);
+
+    // When the stream is done being read, end the response
+    readStream.on('close', () => {
+        res.end()
+    })
+
+    // Stream chunks to response
+    readStream.pipe(res)
+  })
+
 
 	// app.post("/uploadPage/",authed,function(req,res){
 	// 	var text = req.body.text;
