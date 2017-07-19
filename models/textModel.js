f151b50799faf0f0b6a2b73966669a67887bbb06
mmetr14 <mmetr14@freeuni.edu.ge>
2017-07-19 16:11:09 +0400

    fixed crashing when user not authenticated and tries to access uploadfile page

---
+++ models/textModel.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+var textsSchema = new mongoose.Schema({
+  title:String,
+  desciption:String,
+  username:String,
+  filename:String,
+  tags:Array
+});
+
+var textModel = mongoose.model('texts',textsSchema);
