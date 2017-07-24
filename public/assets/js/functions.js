385fbbd4bb8c1b432c9185b5f40a122638682303
IKhaduri <iraklixaduri@gmail.com>
2017-07-25 02:02:22 +0400

    Refactor code

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -1,14 +1,4 @@
 $(document).ready(function(){
-/*
-  $("#uploadid").on('click',function(){
-    $.get("uploadPage").done(function(data){
-      console.log("aqane var");
-      
-      $("#jandaba").html("<div class='content-box' id='content-box-id'></div>");
-      $("#content-box-id").html("&nbsp;"+data);
-    });
-  });
-*/
 $(window).scroll(function() {
     if($(window).scrollTop() == $(document).height() - $(window).height()) {  
         var pathname = window.location.pathname;
